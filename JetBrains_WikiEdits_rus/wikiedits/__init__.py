5ee187e22d79af2b3c03fba34a33ccebc715d538
Iurii Erilov <urezzzer@gmail.com>
2022-09-04 18:17:03 +1000

    Delete JetBrains_WikiEdits_rus directory

--- JetBrains_WikiEdits_rus/wikiedits/__init__.py
+++
@@ -1,7 +0,0 @@
-#  -*- coding: utf-8 -*-
-
-LANGUAGES = [
-    'english',
-    'polish',
-    'german',
-]
--- JetBrains_WikiEdits_rus/wikiedits/wiki/__init__.py
+++
@@ -1,7 +0,0 @@
-#  -*- coding: utf-8 -*-
-
-VANDALISM_REGEXES = {
-    'english': "vandal|stupid|revert",
-    'polish': "anulowan|wycofan|cofnię|cofnie|przywróc|przywroc|revert|rewert",
-    'german': "revert|vandal|rückgängig",
-}
